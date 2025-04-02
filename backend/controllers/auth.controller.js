2d2376cde74d9e31827d21bfface09fb606375af
Paulina282000 <paulina.282000@gmail.com>
2025-04-02 00:49:42 -0300

    Routes principales

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup route call");
+};
+
+export const login = async (req, res) => {
+  res.send("Login route call");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout route call");
+};
